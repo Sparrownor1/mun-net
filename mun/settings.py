33c8b2c52f973fe3a34f1bc4b748bdb8695f80ca
Sachit Lumba <sachitlumba@gmail.com>
2019-11-20 14:45:47 +0400

    Spelling

--- mun/settings.py
+++ mun/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 from decouple import config, Csv
 
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', defaulf=False, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 AUTH_USER_MODEL = 'users.User'
